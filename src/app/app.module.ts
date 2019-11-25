e996e5e77859e65a73bc0817c2ba784f1c161263
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2019-11-26 00:23:25 +0100

    v0.0.5
    Added a few new files for "details" component and updated existed components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddCardComponent } from './add-card/add-card.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     WeatherCardComponent,
-    AddCardComponent
+    AddCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
