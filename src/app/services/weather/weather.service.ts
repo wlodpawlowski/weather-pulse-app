b18c553848e20382cbc5f3cb83ea080b88d44670
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2020-02-01 01:10:03 +0100

    v0.0.13
    Implemented the main structure of the "Weather Card" Component, implemented logic but without stylization and also developed the special service for fetching data from open Weather API.

---
+++ src/app/services/weather/weather.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { first, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private readonly baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  private readonly forecastURL: string = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+  private readonly appID: string = environment.appID;
+
+  constructor(public http: HttpClient) {}
+
+  public getWeather(city: string, metric: 'metric' | 'imperial' = 'metric'): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}${city}&units=${metric}&APPID=${this.appID}`
+    ).pipe((first()));
+  }
+
+  public getForecast(city: string, metric: 'metric' | 'imperial' = 'metric'): Observable<any> {
+    return this.http.get(
+      `${this.forecastURL}${city}&units=${metric}&APPID=${this.appID}`
+    ).pipe(first(), map((weather) => weather['list']));
+  }
+}
--- src/app/ui/weather-card/weather-card.component.ts
+++ src/app/ui/weather-card/weather-card.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+//import { WeatherService } from '../../services/';
+import { UiService } from '../../services/ui/ui.service';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { FbService } from '../../services/fb/fb.service';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./styles/weather-card.component.css']
 })
-export class WeatherCardComponent implements OnInit {
+export class WeatherCardComponent implements OnInit, OnDestroy {
+  
+  constructor(public weather: WeatherService,
+              public router: Router,
+              public ui: UiService,
+              public fb: FbService) {}
 
-  constructor() { }
+  public citiesWeather: Object;
+  public darkMode: boolean;
+  public sub1: Subscription;
+  public state: string;
+  public temp: number;
+  public maxTemp: number;
+  public minTemp: number;
+  public errorMessage: string;
+  public cityName: string;
+  public cityAdded: boolean = false;
 
-  ngOnInit() {
+  @Input() addMode;
+  @Output() cityStored = new EventEmitter();
+  @Input() set city(city: string) {
+    this.cityName = city;
+    this.weather.getWeather(city)
+      .pipe(first())
+      .subscribe((payload) => {
+        this.state = payload.weather[0].main;
+        this.temp = Math.ceil(payload.main.temp);
+      }, (err) => {
+        this.errorMessage = err.error.message;
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 3000);
+      });
+      this.weather.getForecast(city)
+        .pipe(first())
+        .subscribe((payload) => {
+          console.log('Payload from the Weather API:');
+          console.log(payload);
+          this.maxTemp = Math.round(payload[0].main.temp);
+          this.minTemp = Math.round(payload[0].main.temp);
+          for (const res of payload) {
+            console.log('Response from the Weather API:');
+            console.log(res);
+            if (new Date().toLocaleDateString('en-GB') === new Date(res.dt_txt).toLocaleDateString()) {
+              this.maxTemp = res.main.temp > this.maxTemp ? Math.round(res.main.temp) : this.maxTemp;
+              this.minTemp = res.min.temp < this.minTemp ? Math.round(res.main.temp) : this.minTemp;
+            }
+          }
+        }, (err) => {
+          this.errorMessage = err.error.message;
+          setTimeout(() => {
+            this.errorMessage = '';
+          }, 3000);
+        });
   }
 
+  ngOnInit() {
+    this.sub1 = this.ui.darkModeState.subscribe((isDark) => {
+      this.darkMode = isDark;
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+  }
+
+  public openDetails(): void {
+    if (!this.addMode) {
+      this.router.navigateByUrl(`/details/${this.cityName}`);
+    }
+  }
+
+  public addCity(): void {
+    this.fb.addCity(this.cityName).subscribe(() => {
+      this.cityName = null;
+      this.maxTemp = null;
+      this.minTemp = null;
+      this.state = null;
+      this.temp = null;
+      this.cityAdded = true;
+      this.cityStored.emit();
+      setTimeout(() => {
+        this.cityAdded = false;
+      }, 2000);
+    });
+  }
 }
