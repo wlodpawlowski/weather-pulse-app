741b85e484ba8a5bbc615271d352e33d4a2e6703
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2020-03-05 03:19:59 +0100

    v0.0.22
    Completed development of the new service for twitter API, completed develpment of the markup for "Selected City" details component and also started development of the full login for this component.

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WeatherService } from '../../services/weather/weather.service';
+import { forkJoin, Observable, Subscription } from 'rxjs';
+import { UiService } from '../../services/ui/ui.service';
+import { concatMap } from 'rxjs/operators';
+import { TwitterService } from '../../services/twitter/twitter.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./styles/details.component.css']
+  styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DetailsComponent implements OnInit, OnDestroy {
+  constructor() {
+    
   }
 
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
 }
