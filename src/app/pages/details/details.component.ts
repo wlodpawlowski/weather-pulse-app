2518abbb60d3cfadb59a844446b45fe2419537e7
Włodzimierz Pavlov <thepavlovcompany@gmail.com>
2020-03-25 02:06:23 +0100

    v0.0.27
    Completed a full development of the "Selected City" component but without a full bug fixing.

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -38,25 +38,44 @@ export class DetailsComponent implements OnInit, OnDestroy {
     const days = ['Sun', 'Mon', 'Wed', 'Thu', 'Fri', 'Sat'];
     this.today = days[todayNumberInWeek];
     this.sub2 = this.activeRouter.paramMap.pipe(concatMap((route: any) => {
-      console.log('Route Params City:');
-      console.log(route);
       this.city = route.params.city;
       this.cityIllustrationPath = `../../../assets/details/${this.city.toLowerCase()}.png`;
       return forkJoin(this.weather.getWeather(this.city), this.weather.getForecast(this.city)); // forkJoin() function - ??
     })).subscribe((payload) => {
-      console.log(`Payload:\n${payload[0].weather[0].main}`);
       this.state = payload[0].weather[0].main;
       this.temp = Math.ceil(Number(payload[0].main.temp)); // Math.ceil() - Math Library function ??
       this.hum = payload[0].main.humidity;
       this.wind = Math.round(Math.round(payload[0].wind.speed)); // Why we're using round() function twice ?
       const dates = {};
-      console.log('payload[1]:');
-      console.log(payload[1]);
       for (const res of payload[1]) {
+        console.log(res.dt_txt);
         const date = new Date(res.dt_txt).toDateString().split(' ')[0];
-        console.log(`date: ${date}`);
+        console.log(`date: ${date}`); //I'll solve the problem with the daysForecast cards...
+        if (dates[date]) {
+          dates[date].counter += 1;
+          dates[date].temp += res.main.temp;
+        } else {
+          dates[date] = {
+            state: res.weather[0].main,
+            temp: res.main.temp,
+            counter: 1
+          };
+        }
       }
-    })
+      Object.keys(dates).forEach((day) => {
+        dates[day].temp = Math.round(dates[day].temp / dates[day].counter);
+      });
+      delete dates[Object.keys(dates)[0]];
+      this.daysForecast = dates;
+      console.log('daysForecast:');
+      console.log(this.daysForecast);
+    }, (err) => {
+      this.errorMessage = err.error.message;
+      setTimeout(() => {
+        this.errorMessage = '';
+      }, 2500);
+    });
+    this.tweets$ = this.twitter.fetchTweets(this.city);
   }
 
   ngOnDestroy() {
